24f374a2227e6de819a83671a34ac7e278994a94
youniszidan94@gmail.com <youniszidan94@gmail.com>
2019-04-11 04:41:46 +0200

    "Manage_project_edit_del_add"

--- PMS/PMS/Controllers/ProjectController.cs
+++ PMS/PMS/Controllers/ProjectController.cs
@@ -1,7 +1,9 @@
 ﻿using PMS.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -53,6 +55,91 @@ namespace PMS.Controllers
             db.projects.Remove(pros);
             db.SaveChanges();
             return RedirectToAction("ListProjects");
+        }
+        /*********************************************/
+        /*************Manage_project_to_create*************/
+        public ActionResult manage_project()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult manage_project(CreateProject c)
+        {
+            db.CreateProjects.Add(c);
+            db.SaveChanges();
+            return RedirectToAction("ListManageProject");
+        }
+        public ActionResult ListManageProject()
+        {
+            var M = getManageProjects();
+            return View(M);
+        }
+        public IEnumerable<CreateProject> getManageProjects()
+        {
+            var M = db.CreateProjects.ToList();
+            return M;
+        }
+        [HttpGet]
+        public ActionResult Details_project(int id)
+        {
+            var M = getManageProjects().SingleOrDefault(y => y.id == id);
+            return View(M);
+        }
+
+        [HttpGet]
+        public ActionResult DeleteManageProject(int id)
+        {
+            var M = db.CreateProjects.SingleOrDefault(y => y.id == id);
+            return View(M);
+        }
+        [HttpPost]
+        public ActionResult DeleteManageProject(CreateProject pro)
+        {
+            var M = db.CreateProjects.SingleOrDefault(y => y.id == pro.id);
+            db.CreateProjects.Remove(M);
+            db.SaveChanges();
+            return RedirectToAction("ListManageProject");
+        }
+        [HttpGet]
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CreateProject pro = db.CreateProjects.Find(id);
+            if (pro == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pro);
+        }
+
+        [HttpPost, ActionName("Edit")]
+        [ValidateAntiForgeryToken]
+        public ActionResult EditPost(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var Update = db.CreateProjects.Find(id);
+            if (TryUpdateModel(Update, "",
+               new string[] { "pro_name", "strt_date", "end_date","stat","price","leader_name" }))
+            {
+                try
+                {
+                    db.SaveChanges();
+
+                    return RedirectToAction("ListManageProject");
+                }
+                catch (RetryLimitExceededException /* dex */)
+                {
+                    //Log the error (uncomment dex variable name and add a line here to write a log.
+                    ModelState.AddModelError("", "Unable to save changes. Try again, and if the problem persists, see your system administrator.");
+                }
+            }
+            return View(Update);
         }
 
     }
