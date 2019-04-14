66c0f833b2e38507655e5426f21919e7a660cdfd
youniszidan94@gmail.com <youniszidan94@gmail.com>
2019-04-14 06:53:27 +0200

    create_account

--- PMS/PMS/Models/user.cs
+++ PMS/PMS/Models/user.cs
@@ -35,7 +35,7 @@ namespace PMS.Models
         public string email { get; set; }
         public string photo { get; set; }
         public string experience { get; set; }
-    
+        
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<CreateProject> CreateProjects { get; set; }
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
